5cf03e4fad4e413f2a592ad51791f5fa04aadfc3
joe <joe94113@gmail.com>
2022-05-07 17:18:44 +0800

    feat: add pagination have a next or pre judge

--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -1,7 +1,7 @@
 <template>
   <nav aria-label="Page navigation example">
     <ul class="pagination justify-content-center">
-      <li class="page-item">
+      <li class="page-item" :class="{ disabled: !pages.has_pre }">
         <a class="page-link" href="#" aria-label="Previous">
           <span aria-hidden="true">&laquo;</span>
         </a>
@@ -16,7 +16,7 @@
           {{ page }}
         </a>
       </li>
-      <li class="page-item">
+      <li class="page-item" :class="{ disabled: !pages.has_next }">
         <a class="page-link" href="#" aria-label="Next">
           <span aria-hidden="true">&raquo;</span>
         </a>
